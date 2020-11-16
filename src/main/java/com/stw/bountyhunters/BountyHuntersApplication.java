d25bac7787b96eb6bd8c1da775539817b78c0519
pandrzejak <piotr.andrzejak@primaris.eu>
2020-11-16 19:33:52 +0100

    Initial COmmit

---
+++ src/main/java/com/stw/bountyhunters/BountyHuntersApplication.java
@@ -0,0 +1,13 @@
+package com.stw.bountyhunters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BountyHuntersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BountyHuntersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stw/bountyhunters/BountyHuntersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stw.bountyhunters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BountyHuntersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
