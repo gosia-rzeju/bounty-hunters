6238671ca980bcddd5c08ae8fd96370df5c56278
pandrzejak <piotr.andrzejak@primaris.eu>
2020-11-18 17:56:31 +0100

    adding bounty hunters index page and controller

--- src/main/java/com/stw/bountyhunters/controllers/IndexPageController.java
+++ src/main/java/com/stw/bountyhunters/controllers/IndexPageController.java
@@ -1,12 +1,7 @@
 package com.stw.bountyhunters.controllers;
 
-import com.stw.bountyhunters.model.BountyHunter;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class IndexPageController {
@@ -16,28 +11,5 @@ public class IndexPageController {
         return "index";
     }
 
-    @RequestMapping("/bountyHunters")
-    public String getBountyHunters(Model model) {
 
-        List<BountyHunter> bountyHunters = new ArrayList<>();
-
-        BountyHunter bountyHunter1 = new BountyHunter();
-        bountyHunter1.setLogin("Rzeju");
-        bountyHunter1.setEmail("pandrzejak93@gmail.com");
-        bountyHunter1.setPower(10000L);
-        bountyHunter1.setMoney(100L);
-
-        BountyHunter bountyHunter2 = new BountyHunter();
-        bountyHunter2.setLogin("Gosia");
-        bountyHunter2.setEmail("gosia@gmail.com");
-        bountyHunter2.setPower(100000L);
-        bountyHunter2.setMoney(1000L);
-
-        bountyHunters.add(bountyHunter1);
-        bountyHunters.add(bountyHunter2);
-
-        model.addAttribute("hunters", bountyHunters);
-
-        return "index";
-    }
 }
